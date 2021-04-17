c2f3a2e6c59ff2198a6cd7ed35dfe483f3d720c8
peter huyser <huyserp@gmail.com>
2021-04-17 12:38:39 +0200

    add user reference in chatroom, chatroom belongs_to user

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,5 @@
 class Chatroom < ApplicationRecord
+  belongs_to :user
   has_many :messages
 
   validates :topic, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_101308) do
+ActiveRecord::Schema.define(version: 2021_04_17_103636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(version: 2021_04_17_101308) do
     t.string "topic"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_chatrooms_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "chatroom_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +46,7 @@ ActiveRecord::Schema.define(version: 2021_04_17_101308) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chatrooms", "users"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
 end

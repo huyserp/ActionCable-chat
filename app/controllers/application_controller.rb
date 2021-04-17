d112116de07a325a2d54f47efdcde7fdeb4bd328
peter huyser <huyserp@gmail.com>
2021-04-17 15:17:04 +0200

    installed and configured pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  include Pundit
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -9,5 +10,13 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+  end
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end

2f2c6b11aa244ecd9eb30470f39fc45fe964d7d2
peter huyser <huyserp@gmail.com>
2021-04-17 16:05:16 +0200

    defined chatroom actions

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,35 @@
 class ChatroomsController < ApplicationController
+  before_action :find_chatroom, only: [:show, :destroy]
+
   def index
     @chatrooms = policy_scope(Chatroom).order(created_at: :desc)
   end
 
+  def show
+    authorize @chatroom
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    @chatroom.user = current_user
+    if @chatroom.save
+      redirect_to chatroom_path(@chatroom)
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    authorize @chatroom
+  end
+
+  private
+
+  def find_chatroom
+    @chatroom = Chatroom.find(params[:id])
+  end
+
+  def chatroom_params
+    params.require(:chatroom).permit(:topic)
+  end
 end

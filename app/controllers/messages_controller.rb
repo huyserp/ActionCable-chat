102331f794506e6358dcb2e3880bd544411c9004
peter huyser <huyserp@gmail.com>
2021-04-18 14:02:06 +0200

    create messages controllor with routed actions and form to send message in chatroom

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,25 @@
+class MessagesController < ApplicationController
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(message_params)
+    @message.user = current_user
+    @message.chatroom = @chatroom
+    if @message.save
+      redirect_to chatroom_path(@chatroom)
+    else
+      render 'chatrooms/show'
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end

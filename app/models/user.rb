6217c4bda59835c9987676ecb7da01dd19b01965
peter huyser <huyserp@gmail.com>
2021-05-22 14:20:33 +0200

    changed available colors and update db

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,11 @@ class User < ApplicationRecord
   has_many :chatrooms
   has_many :messages
 
-  COLORS = ['54478C', '2C699A', '048BA8',
-            '0DB39E', '16DB93', '83E377',
-            'B9E769', 'EFEA5A', 'F1C453', 'F29E4C']
+  COLORS = ['0DB39E', '16DB93', '83E377', 'B9E769', 'EFEA5A', 'F1C453', 'F29E4C']
 
   validates :nickname, presence: true
   validates :color, presence: true, inclusion: { in: COLORS }
 end
+
+
+# '54478C', '2C699A', '048BA8',

e7f9104514035d0f1377148db1b539fc4f06361a
peter huyser <huyserp@gmail.com>
2021-06-12 15:23:15 +0200

    user can add and update photo for profile, displays in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,17 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit
 
-  def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :color])
-
-    # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :color])
-  end
+  # Pundit white-list
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :color, :photo])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :color, :photo])
+  end
 
   private
 
